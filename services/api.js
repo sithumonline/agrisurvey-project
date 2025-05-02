29be3baffacd78b8b71ecb01f0890e0c7eef4ed4
Sithum Bopitiya <sithumsandeepap@gmail.com>
2025-05-02 10:04:33 +0530

    Init action

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 // frontend/src/services/api.js
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://agri-survey-server.fly.dev/api/';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api';
 
 // Create axios instance with baseURL
 const apiClient = axios.create({
